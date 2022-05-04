99e12d55bf793bc71302e5829d7bcffe995e037d
Ed Hulbert <edward.hulbert@hotmail.co.uk>
2022-05-04 09:04:06 +0100

    first commit

---
+++ src/main/java/org/example/streamapi/Extension.java
@@ -0,0 +1,78 @@
+package org.example.streamapi;
+
+import org.example.streamapi.model.Bodybuilder;
+import org.example.streamapi.model.Friend;
+import org.example.streamapi.model.User;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
+public class Extension {
+    /*
+        Given two int numbers a and b, return int [] with values that are in the range between smaller and bigger arg:
+        - use IntStream.range
+        - swap the argument's values without introducing a new, local variable.
+    */
+    public int [] streamNumbers(int a, int b) {
+        // Implement me :)
+        if (a > b) {
+            return IntStream.range(b,a).toArray();
+        } else {
+            return IntStream.range(a,b).toArray();
+        }
+    }
+
+    /*
+        Given a list of users, return a User with the given user id. If there is no user with this id,
+        return new user with the name "New user", given id, gender "unknown".
+
+        (use Optional API)
+    */
+    public User getUserByIdOrCreateNew(List<User> users, long userId) {
+        // Implement me :)
+//        for (User user : users) {
+//            if (user.getId() == (userId)){
+//                return user;
+//            } else {
+//                return user;
+//            }
+//        }
+
+//        Optional<Long> userIdOptional = Optional.of(userId);
+//        if (userIdOptional.isPresent()) {
+//            return users.stream().filter(el->el.getId()==userId).
+//        }
+        return null;
+    }
+
+    /*
+        Given List<Friend>, filter the ones who are available on Saturday and want to party:
+        - getAvailableDay returns "Saturday" and
+        - getActivity returns "Party"
+        - define predicates and use '.and' method.
+    */
+
+    public List<String> partyWithFriends(List<Friend> friends) {
+        // Implement me :)
+        return null;
+    }
+
+    /*
+        Return names of sorted bodybuilders.
+
+        Sort List<Bodybuilder> using your custom comparator.
+
+        Write a comparator for type BodyBuilder that will sort bodybuilders by:
+        - who can lift more,
+        - then who is younger,
+        - then name alphabetically.
+     */
+    public List<String> sortBodybuilders(List<Bodybuilder> bodybuilders) {
+        // Implement me :)
+        return null;
+    }
+
+}
---
+++ src/main/java/org/example/streamapi/MVP.java
@@ -0,0 +1,74 @@
+package org.example.streamapi;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Locale;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
+public class MVP {
+
+    /*
+        Convert a List<String> to an object stream and for each element -> System,out.println.
+     */
+
+    public void printNames(List<String> names) {
+        // Implement me :)
+        names.stream().forEach(people -> System.out.println(people));
+    }
+
+    /*
+        Given a List<Integers>, return a List<Integer> with event numbers.
+     */
+    public List<Integer> returnEvenNumbers(List<Integer> numbers) {
+        // Implement me :)
+        return numbers.stream()
+                .filter(num -> num % 2 == 0)
+                .toList();
+    }
+
+    /*
+        Given an int [], double the value of each int and return an int [].
+    */
+    public int[] doubleInts(int[] numbers) {
+        // Implement me :)
+        return Arrays.stream(numbers).map(num -> num * 2).toArray();
+    }
+
+    /*
+        Given a String, return a List<String>, all caps.
+     */
+    public List<String> splitToAllCapsList(String input) {
+        // Implement me :)
+        return Arrays.stream(input.split("")).map(el -> el.toUpperCase()).toList();
+    }
+
+
+    /*
+    Given a list of animals:
+     - filter the ones that start with a given letter
+     - return sorted List<String>, all caps.
+    */
+    public List<String> filterByFirstLetterAndOrder(List<String> list, String letter) {
+        // Implement me :)
+        return list.stream()
+                .filter(el -> Character.toString(el.charAt(0)).equals(letter))
+                .map(el -> el.toUpperCase())
+                .sorted()
+                .toList();
+    }
+
+    /*
+        Given a list of words, return elements that:
+         - are shorter than the given number and
+         - start with a given letter.
+    */
+    public List<String> filterWords(List<String> words, int maxLength, String firstLetter) {
+        // Implement me :)
+        return words.stream()
+                .filter(el -> el.length() <= maxLength
+                    && Character.toString(el.charAt(0)).equals(firstLetter))
+                .toList();
+    }
+}
